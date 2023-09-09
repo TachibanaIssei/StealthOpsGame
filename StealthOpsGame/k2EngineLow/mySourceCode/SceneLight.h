03c711177d825a73454e79a45ace94dfe7877876
Tachibana <tatibana.issei.kawahara@gmail.com>
2023-09-09 15:11:33 +0900

    SceneLightクラスにディレクションライトの情報を作成

--- StealthOpsGame/k2EngineLow/mySourceCode/SceneLight.h
+++ StealthOpsGame/k2EngineLow/mySourceCode/SceneLight.h
@@ -15,10 +15,46 @@ namespace nsK2EngineLow
 
 	struct Light
 	{
-		DirectionalLight directionalLight[MAX_DIRECTIONAL_LIGHT];
+		DirectionalLight directionalLight[MAX_DIRECTIONAL_LIGHT];	//ディレクショナルライトの配列
+		Vector3 eyePos;			//カメラの位置
+		float pad0;				//パディング0
+		Vector3 ambientLight;	//環境光
+		float pad1;				//パディング1
 	};
 
-	class SceneLight
+	class SceneLight : public Noncopyable
 	{
+	public:
+		SceneLight() {};
+		~SceneLight() {};
+
+		/// <summary>
+		/// 初期化
+		/// </summary>
+		void Init();
+		/// <summary>
+		/// シーンライトを取得
+		/// </summary>
+		/// <returns></returns>
+		Light& GetSeneLight()
+		{
+			return m_light;
+		}
+		void SetDirectionLight(int lightNo, Vector3 direction, Vector4 color)
+		{
+			m_light.directionalLight[lightNo].direction = direction;
+			m_light.directionalLight[lightNo].color		= color;
+		}
+		bool IsCastShadow(int lightNo)
+		{
+			return m_light.directionalLight[lightNo].castShadow;
+		}
+		void SetAmbient(Vector3 ambient)
+		{
+			m_light.ambientLight = ambient;
+		}
+
+	private:
+		Light m_light;  //シーンライト
 	};
 }
