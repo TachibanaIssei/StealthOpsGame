8e604798c43721ffbe37568817ed8f8e876ed147
tachibana <issei.misasagi.3691@gmail.com>
2023-09-19 13:47:59 +0900

    川瀬式ブルームを作成した

--- StealthOpsGame/k2EngineLow/mySourceCode/PostEffect/Bloom.h
+++ StealthOpsGame/k2EngineLow/mySourceCode/PostEffect/Bloom.h
@@ -4,5 +4,35 @@ namespace nsK2EngineLow
 {
 	class Bloom : public Noncopyable
 	{
+	public:
+		void Init(RenderTarget& mainRenderTarget);
+		void OnRender(RenderContext& rc, RenderTarget& mainRenderTarget);
+
+	private:
+		/// <summary>
+		/// レンダリングターゲットの初期化
+		/// </summary>
+		/// <param name="mainRenderTarget">メインレンダリングターゲット</param>
+		void InitRenderTarget(const RenderTarget& mainRenderTarget);
+		/// <summary>
+		/// 輝度抽出用のスプライトの初期化
+		/// </summary>
+		/// <param name="mainRenderTarget">メインレンダリングターゲット</param>
+		void InitLuminanceSprite(RenderTarget& mainRenderTarget);
+		/// <summary>
+		/// ボケ画像を加算合成するスプライトの初期化
+		/// </summary>
+		/// <param name="mainRenderTarget">メインレンダリングターゲット</param>
+		void InitFinalSprite(const RenderTarget& mainRenderTarget);
+		/// <summary>
+		/// ガウシアンブラーの初期化
+		/// </summary>
+		void InitGaussianBlur();
+
+	private:
+		RenderTarget				m_luminnceRenderTarget;	//輝度抽出用レンダリングターゲット
+		Sprite						m_luminanceSprite;		//輝度抽出画像
+		Sprite						m_finalSprite;			//最終画像
+		std::array<GaussianBlur, 4>	m_gaussianBlur;			//ガウシアンブラー
 	};
 }
