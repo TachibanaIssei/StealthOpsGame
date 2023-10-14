123e83aa424f72f074aad54955b4918ec3548e7f
tachibana <issei.misasagi.3691@gmail.com>
2023-10-14 16:59:16 +0900

    ゲームクラスを作成した

--- StealthOpsGame/Game/GameCamera.h
+++ StealthOpsGame/Game/GameCamera.h
@@ -1,5 +1,19 @@
 ﻿#pragma once
+#include "../k2EngineLow/mySourceCode/Camera/SpringCamera.h"
+
 class GameCamera : public IGameObject
 {
+public:
+	GameCamera() {};
+	~GameCamera() {};
+	bool Start() override final;
+	void Update() override final;
+
+private:
+	SpringCamera m_springCamera;
+
+	Vector3 m_position = Vector3::Zero;
+	Vector3 m_target = Vector3::Zero;
+	Vector3 m_toCameraPos = Vector3::Zero;
 };
 
